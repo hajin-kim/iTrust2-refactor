39cb70f8736830373b83aa5998d4c21f525dbef8
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-10-29 17:21:13 +0900

    ➕ Add Spring Boot Validation

--- iTrust2/build.gradle.kts
+++ iTrust2/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("org.hibernate:hibernate-validator:6.1.5.Final")
