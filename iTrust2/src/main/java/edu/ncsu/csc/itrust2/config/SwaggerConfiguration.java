f78a7170510abcb43dcd0921127aabdfad05ee82
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-06 21:23:20 +0900

    :memo: Modify Swagger description

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/config/SwaggerConfiguration.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/config/SwaggerConfiguration.java
@@ -1,9 +1,7 @@
 package edu.ncsu.csc.itrust2.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,15 +9,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfiguration {
     @Bean
     public OpenAPI api() {
-        SecurityScheme auth =
-                new SecurityScheme()
-                        .type(SecurityScheme.Type.APIKEY)
-                        .in(SecurityScheme.In.COOKIE)
-                        .name("JSESSIONID");
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("basicAuth");
+        var info =
+                new Info()
+                        .description(
+                                """
+                        ### 인증
 
-        return new OpenAPI()
-                .components(new Components().addSecuritySchemes("basicAuth", auth))
-                .addSecurityItem(securityRequirement);
+                        1. 다른 탭에 iTrust2를 띄웁니다.
+                        2. 로그인합니다.
+                        3. 끝입니다. 로그인 정보가 그대로 적용됩니다.
+
+                        [GET /api/v1/role](/iTrust2/swagger-ui/index.html#/api-user-controller/getRole) 을 실행해보시면 로그인한 계정의 권한이 그대로 표시되는 것을 확인하실 수 있습니다.
+                        """
+                                        .trim());
+
+        return new OpenAPI().info(info);
     }
 }
